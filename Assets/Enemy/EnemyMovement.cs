b0004743f8337d5f1688335f8c245d99e3ddc0fd
maorroz <andory@post.bgu.ac.il>
2019-03-10 03:17:40 +0200

    added basic collision

---
+++ Assets/Enemy/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D enemyBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyBody = GetComponent<Rigidbody2D>();
+        enemyBody.isKinematic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        enemyBody.constraints = RigidbodyConstraints2D.FreezeAll;
+        Debug.Log("OnCollisionEnter2D Enemy");
+    }
+}
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 0.025f;
-
+    public float speed = 1.2f;
     public float moveVertical = 0f;
-
     public float moveHorziontal = 0f;
-
-
-    private CharacterController characterController;
-
+    private Rigidbody2D playerBody;
     private PlayerHealth playerHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        playerBody = GetComponent<Rigidbody2D>();
+        playerBody.isKinematic = false;
         var healthBar = this.gameObject.transform.GetChild(1);
         playerHealth = healthBar.GetComponent<PlayerHealth>();
     }
 
     void FixedUpdate(){
-        Vector2 movement = new Vector2(moveHorziontal,moveVertical);
-        characterController.Move(movement);
+        Vector2 movement = new Vector2(moveHorziontal,moveVertical) * speed;
+        playerBody.velocity = movement;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isDead = playerHealth.hp <= 0;
-        if(isDead){
-            return;
-        }
-        moveVertical = Input.GetAxisRaw("Vertical") * speed;
-        moveHorziontal = Input.GetAxisRaw("Horizontal") * speed;
+        bool isDying = playerHealth.hp <= 0;
+        moveVertical = isDying ? 0 : Input.GetAxisRaw("Vertical");
+        moveHorziontal = isDying ? 0 : Input.GetAxisRaw("Horizontal");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        Debug.Log("OnCollisionEnter2D player");
+    }
+
+        void OnCollisionExit2D()
+    {
+        Debug.Log("OnCollisionExit2D player");
     }
 }
