8f2d988f39538622378213a27277f173a81ea4a6
maorroz <andory@post.bgu.ac.il>
2019-07-09 17:35:15 +0300

    added pause menu ui

---
+++ Assets/UI/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+
+    public void ExitGame(){
+        Application.Quit(0);
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+}
