f632f0835662ab69a987b3690fc370c7075fe570
Igor8757 <igor8757@gmail.com>
2019-07-20 16:00:05 +0300

    added BGM + hit sound + First hall walls

--- Assets/Player/PlayerAttackSystem.cs
+++ Assets/Player/PlayerAttackSystem.cs
@@ -7,21 +7,28 @@ public class PlayerAttackSystem : MonoBehaviour
     private MeleeTargets meleeTargets;
     private HealthSystem health;
     private PlayerStatus status;
+
+    public AudioSource hitSound;
     // Start is called before the first frame update
     void Start()
     {
         meleeTargets = GetComponent<MeleeTargets>();
         health = GetComponent<HealthSystem>();
         status = GetComponent<PlayerStatus>();
+        hitSound = GetComponent<AudioSource>();
     }
 
     void MeleeAttack()
     {
         List<HealthSystem> targets = meleeTargets.Targets;
+        bool isHit = false;
         foreach (HealthSystem target in targets)
         {
             target.DecreaseHealth(13);
+            isHit = true;
         }
+        if(isHit)
+            hitSound.Play();
     }
 
     void Update()
