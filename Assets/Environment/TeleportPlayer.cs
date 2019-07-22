7221ba3d547731f3a5c12c7fbec4bb217137184c
MaorRoz <andory@post.bgu.ac.il>
2019-07-23 00:27:06 +0300

    fixed healthbar scaling

--- Assets/Environment/TeleportPlayer.cs
+++ Assets/Environment/TeleportPlayer.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class TeleportPlayer : MonoBehaviour
 {
-    public GameObject player;
-    
-    void OnTriggerEnter2D(Collider2D otherCollider)
-    {
-        if (otherCollider.tag == "Player")
-        {
-            player.transform.position = new Vector3(25.12f, -1.11f, 0.0f);
-        }
+  public GameObject player;
 
+  void OnTriggerEnter2D(Collider2D otherCollider)
+  {
+    if (otherCollider.tag == "Player")
+    {
+      player.transform.position = new Vector3(25.12f, -1.11f, 0.0f);
     }
+
+  }
 }
--- Assets/General/PersonalHealthUI.cs
+++ Assets/General/PersonalHealthUI.cs
@@ -4,48 +4,49 @@ using UnityEngine;
 
 public class PersonalHealthUI : MonoBehaviour
 {
-    private HealthText healthText;
-    private Transform healthBarTransform;
+  private HealthText healthText;
+  private Transform healthBarTransform;
 
-    private Vector3 healthBarScaleVector;
-    private Vector2 healthBarPositionVector;
-    private float maxScaleX;
-    // Start is called before the first frame update
-    void Start()
-    {
-        healthBarTransform = gameObject.transform.Find("HealthBar");
-        healthBarPositionVector = healthBarTransform.localPosition;
-        healthBarScaleVector = healthBarTransform.localScale;
-        maxScaleX = healthBarTransform.localScale.x;
+  private Vector3 healthBarScaleVector;
+  private Vector2 healthBarPositionVector;
+  private float maxScaleX;
+  // Start is called before the first frame update
+  void Start()
+  {
+    healthBarTransform = gameObject.transform.Find("HealthBar");
+    healthBarPositionVector = healthBarTransform.localPosition;
+    healthBarScaleVector = healthBarTransform.localScale;
+    maxScaleX = healthBarTransform.localScale.x;
 
-        healthText = gameObject.transform.Find("HpTextCanvas/HpText").GetComponent<HealthText>();
-    }
+    healthText = gameObject.transform.Find("HpTextCanvas/HpText").GetComponent<HealthText>();
+  }
 
-    private void DecreaseHealthBar(int maxHp, int currentHp)
-    {
-        float healthPercentage = (currentHp % maxHp) * 0.01f;
-        healthBarScaleVector.x = maxScaleX * healthPercentage;
-        healthBarPositionVector.x = 0.5f * (healthBarScaleVector.x - maxScaleX);
+  private void DecreaseHealthBar(int maxHp, int currentHp)
+  {
+    float healthPercentage = (currentHp * 100.0f / maxHp) * 0.01f;
+    Debug.Log(healthPercentage);
+    healthBarScaleVector.x = maxScaleX * healthPercentage;
+    healthBarPositionVector.x = 0.5f * (healthBarScaleVector.x - maxScaleX);
 
-        healthBarTransform.localScale = healthBarScaleVector;
-        healthBarTransform.localPosition = healthBarPositionVector;
-    }
+    healthBarTransform.localScale = healthBarScaleVector;
+    healthBarTransform.localPosition = healthBarPositionVector;
+  }
 
-    private void updateHealthText(int maxHp, int currentHp)
-    {
-        healthText.currentHp = currentHp;
-        healthText.maxHp = maxHp;
-    }
+  private void updateHealthText(int maxHp, int currentHp)
+  {
+    healthText.currentHp = currentHp;
+    healthText.maxHp = maxHp;
+  }
 
-    public void UpdateHealth(int maxHp, int currentHp)
-    {
-        DecreaseHealthBar(maxHp, currentHp);
-        updateHealthText(maxHp, currentHp);
-    }
+  public void UpdateHealth(int maxHp, int currentHp)
+  {
+    DecreaseHealthBar(maxHp, currentHp);
+    updateHealthText(maxHp, currentHp);
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
+  // Update is called once per frame
+  void Update()
+  {
 
-    }
+  }
 }
