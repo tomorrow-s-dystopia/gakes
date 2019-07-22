94265d754b0e0234d8e084996585839bde5e24c2
MaorRoz <andory@post.bgu.ac.il>
2019-07-23 00:04:39 +0300

    added teleporter

---
+++ Assets/Environment/TeleportPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.tag == "Player")
+        {
+            player.transform.position = new Vector3(25.12f, -1.11f, 0.0f);
+        }
+
+    }
+}
