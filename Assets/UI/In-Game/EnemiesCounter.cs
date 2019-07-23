a8c0f3be43abfe84ee5dd15237b52719472ad5bd
MaorRoz <andory@post.bgu.ac.il>
2019-07-23 23:42:53 +0300

    added enemy counter

---
+++ Assets/UI/In-Game/EnemiesCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemiesCounter : MonoBehaviour
+{
+    private TextMeshProUGUI textObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textObject = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyStatus[] enemies = GameObject.FindObjectsOfType<EnemyStatus>();
+        int numberOfLiveEnemies = 0;
+        foreach(EnemyStatus enemy in enemies){
+            if(!enemy.isDead){
+                numberOfLiveEnemies++;
+            }
+        }
+        textObject.SetText("Enemis Left : {0}", numberOfLiveEnemies);
+    }
+}
